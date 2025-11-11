bdb134d449cb28c3300fd0267069660c52749ace
Tameem Ramadan <tamimhesham4@gmail.com>
2025-11-11 09:25:53 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -3,15 +3,26 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-class person{
+class course{
 private:
-string name;
-int id;
-
+    string courseCode;
+    string courseName;
+    int maxStudent;
+    Student* students;
+    int currentStudents;
 public:
-void display()
-cout<<"name: "<<name<<"id: "<<id<<endl;
-};
+    course(string cc,string cn,int ms,Student* s,int cs){
+   courseCode = cc;
+   courseName = cn;
+   maxStudent = ms;
+   students = s;
+   currentStudents = cs;
+   
+~
+   }
+   
+   
+   };
 
 
 #endif
